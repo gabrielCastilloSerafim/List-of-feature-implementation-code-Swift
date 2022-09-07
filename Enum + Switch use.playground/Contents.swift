9bc2ee81063473fc19095104e61a62faa7447ab6
gabrielCastilloSerafim <castillo.serafim.gabriel@gmail.com>
2022-09-07 09:17:29 +0200

    add Enum + Switch use file

---
+++ Enum + Switch use.playground/Contents.swift
@@ -0,0 +1,19 @@
+//Example of enum and switch statement use
+
+enum socialMedia {
+    case twitter
+    case faceBook
+    case instagram
+}
+
+func shareImage (platform: socialMedia) {
+    switch platform {
+        
+    case .twitter:
+        //code
+    case .faceBook:
+        //code
+    case .instagram:
+        //code
+    }
+}
